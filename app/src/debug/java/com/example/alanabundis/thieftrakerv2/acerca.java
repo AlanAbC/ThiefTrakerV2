83d7c8cbf29d8560b95a93bdbf6d257d11b8da14
AlanAbC <alanjiraya77@gmail.com>
2017-04-05 16:29:28 -0500

    Se agrego toda la parte visual

---
+++ app/src/debug/java/com/example/alanabundis/thieftrakerv2/acerca.java
@@ -0,0 +1,14 @@
+package com.example.alanabundis.thieftrakerv2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class acerca extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_acerca);
+    }
+}
