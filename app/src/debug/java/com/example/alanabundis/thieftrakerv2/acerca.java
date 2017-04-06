56b80db7f7dace25088a8a19499792a0c3ed2161
Ricardo Antonio Pizano Pérez <rikymon2@hotmail.com>
2017-04-06 16:05:28 -0500

    Se agrego el redireccionamiento a la cuenta de paypal al dar click en paypal del activity paypal

--- app/src/debug/java/com/example/alanabundis/thieftrakerv2/acerca.java
+++ app/src/debug/java/com/example/alanabundis/thieftrakerv2/acerca.java
@@ -1,7 +1,11 @@
 package com.example.alanabundis.thieftrakerv2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class acerca extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class acerca extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_acerca);
+        ImageView paypal = (ImageView)findViewById(R.id.paypal);
+        paypal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=85QFE5ZJLM4DL");
+                Intent i = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(i);
+            }
+        });
     }
 }
