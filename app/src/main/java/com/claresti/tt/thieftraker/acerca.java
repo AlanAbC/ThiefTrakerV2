64d57e2b74feea1bdc81eca6d35edb6f9dcc39a9
Ricardo Antonio Pizano Pérez <rikymon2@hotmail.com>
2017-04-22 21:40:37 -0500

    se quito el boton de donativos

--- app/src/main/java/com/claresti/tt/thieftraker/acerca.java
+++ app/src/main/java/com/claresti/tt/thieftraker/acerca.java
@@ -14,14 +14,5 @@ public class acerca extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_acercade);
-        ImageView paypal = (ImageView)findViewById(R.id.paypal);
-        paypal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=85QFE5ZJLM4DL");
-                Intent i = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(i);
-            }
-        });
     }
 }
